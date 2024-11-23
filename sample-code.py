a229ad0cdbd5f2c396bed83202b0e13d1906c032
Aman Kumar <coolpadcoolaman@gmail.com>
2024-11-23 17:29:56 +0530

    i am

--- sample-code.py
+++ sample-code.py
@@ -4,4 +4,7 @@ love_message = "I ❤️ YOU"
 
 # Create a pyramid of love messages
 for i in range(1, 6):  # Adjust the range to change the pyramid height
-    print(f"{love_message} " * i)
+    print(f"{love_message} " * 
+
+
+i am done some new changage.
